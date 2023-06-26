c4d7078c1e296b53f7a3dcc09f00701f0cb75dda
Patrick <malamashangala13@gmail.com>
2023-06-26 20:42:57 +0200

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="container">
-    <Header title="Task tracker" />
-    <AddTask @add-task="addTask" />
+    <Header @toggle-add-task="toggleAddTask" title="Task tracker" :showAddTask="showAddTask" />
+    <div v-show="showAddTask"> 
+      <AddTask @add-task="addTask" />
+    </div>
+   
     <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
   </div>
 </template>
@@ -20,10 +23,19 @@ export default {
   },
   data() {
     return {
-      tasks: []
+      tasks: [],
+      showAddTask: false
     }
   },
   methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask 
+    },
+    addTask(task){
+      this.tasks = [...this.tasks, task]
+    },
+
+
     deleteTask(id) {
       if (confirm('Are you sure?')) {
         this.tasks = this.tasks.filter((task)=> task.id !== id)
